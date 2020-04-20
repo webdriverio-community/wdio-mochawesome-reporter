b41ef17ab4e7547b569d670cffafd260f3d71079
Arnis Gustins <arnis.gustins@testdevlab.com>
2020-04-20 14:20:47 +0300

    Add back addTestContext() function in test.js

--- src/test.js
+++ src/test.js
@@ -17,7 +17,7 @@ module.exports = class {
         this.parentUUID = suiteUUID
         this.skipped = false
         this.isHook = false
-        this.context = []
+        this.context = addTestContext(data) // see below
         this.state = ''
         this.err = {}
     }
@@ -42,8 +42,8 @@ module.exports = class {
         /**
          * Jest Matchers returns error strings with ansi color information
          * https://github.com/jest-community/vscode-jest/issues/279
-         * 
-         * stripAnsi will remove those colors.  
+         *
+         * stripAnsi will remove those colors.
          * Tested with Jest Matchers AND Chai (which does not include the ansi characters)
          */
         if (result.error) {
@@ -72,4 +72,25 @@ module.exports = class {
             value: `data:image/jpeg;base64,${value}`
         })
     }
+}
+
+/**
+* Mochawesome has a utility that allows for adding a context key
+* directly to a mocha test object.  If those exist this will add them
+* https://github.com/adamgruber/mochawesome#example
+*
+* putting this outside the class b/c it shouldn't be called directly
+*/
+function addTestContext (data) {
+    let testContext = []
+    if (data.context) {
+        if (Array.isArray(data.context)) {
+            data.context.forEach((ctx) => {
+                testContext.push(ctx)
+            })
+        } else {
+            testContext.push(data.context)
+        }
+    }
+    return testContext
 }
